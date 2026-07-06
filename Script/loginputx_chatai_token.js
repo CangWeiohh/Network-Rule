c9a18a496e0183acc6364a91de46e2d386efbe62
cangwei <@>
2026-07-06 16:43:31 +0800

    update

--- Script/loginputx_chatai_token.js
+++ Script/loginputx_chatai_token.js
@@ -10,9 +10,7 @@ console.log("\n");
 console.log("\n");
 
 const url =
-  "https://api.day.app/wpgnnhHjPuze3cscmxSFyU/ChataiToken/" +
-  encodeURIComponent(token) +
-  "?autoCopy=1&automaticallyCopy=1";
+  "https://api.day.app/wpgnnhHjPuze3cscmxSFyU/落格AI问答Token/长按复制?icon=https://txc.gtimg.com/data/314521/2021/0312/75314428dfa675404a6d71dbb2571acb.png&autoCopy=1&automaticallyCopy=1&copy=" + encodeURIComponent(token);
 
 // ✅ Loon 正确网络请求方式（关键）
 $httpClient.get(url, function (err, resp, data) {
