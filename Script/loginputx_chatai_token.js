b9d7a2c54249c836e4cfc5e1c370fce25b7acceb
cangwei <@>
2026-07-06 16:47:48 +0800

    add

--- Script/loginputx_chatai_token.js
+++ Script/loginputx_chatai_token.js
@@ -10,7 +10,7 @@ console.log("\n");
 console.log("\n");
 
 const url =
-  "https://api.day.app/wpgnnhHjPuze3cscmxSFyU/落格AI问答Token/长按复制?icon=https://txc.gtimg.com/data/314521/2021/0312/75314428dfa675404a6d71dbb2571acb.png&autoCopy=1&automaticallyCopy=1&copy=" + encodeURIComponent(token);
+  "https://api.day.app/7ezfHKp8XLhpuR5Gkb3PUU/落格AI问答Token/长按复制?icon=https://txc.gtimg.com/data/314521/2021/0312/75314428dfa675404a6d71dbb2571acb.png&autoCopy=1&automaticallyCopy=1&copy=" + encodeURIComponent(token);
 
 // ✅ Loon 正确网络请求方式（关键）
 $httpClient.get(url, function (err, resp, data) {
@@ -25,3 +25,4 @@ $httpClient.get(url, function (err, resp, data) {
 console.log(token);
 
 $done({});
+
